32712f558dc90a83ad30c7d201c069c5da79590a
lemon-lime-honey <bs0308@g.skku.edu>
2023-04-28 17:21:15 +0900

    feat urls for places app

--- places/urls.py
+++ places/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 app_name = 'places'
 urlpatterns = [
+    path('', views.place_list),
+    path('<int:place_pk>/', views.place_detail),
+    path('<int:place_pk>/reviews/', views.review_list),
+    path('<int:place_pk>/reviews/<review_pk>/', views.review_detail),
 ]
