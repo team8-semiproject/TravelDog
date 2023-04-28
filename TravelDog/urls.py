cd755aeb6a928449f9de3880c366bbed3e462b44
lemon-lime-honey <bs0308@g.skku.edu>
2023-04-28 11:22:00 +0900

    Add base code

---
+++ TravelDog/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Snippets API',
+        defualt_version='v1',
+        description='Test description',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='contact@snippets.local'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger')),
+    path('accounts/', include('accounts.urls')),
+    path('places/', include('places.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
