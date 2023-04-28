6b5c5b3d65c24c6b227537a517f67eecbea3241a
Shureeshu <ruby04.s.lee@gmail.com>
2023-04-28 16:42:11 +0900

    feat models for accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
+from django.apps import apps
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        """
+        Create and save a user with the given username and password.
+        """
+        if not username:
+            raise ValueError('The given username must be set')
+
+        GlobalUserModel = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password):
+        user = self.create_user(
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+class User(AbstractBaseUser):
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=30,
+        unique=True,
+        help_text=_('Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
