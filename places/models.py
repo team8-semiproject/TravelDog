f51aaaaf0906aade018da8c128518e5d3384c375
lemon-lime-honey <bs0308@g.skku.edu>
2023-04-28 15:32:20 +0900

    feat models for places app

--- places/models.py
+++ places/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from accounts import User
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longtitude = models.FloatField()
+    bookmark = models.ManyToManyField(User, related_name='bookmarked_places')
+
+
+class Photo(models.Model):
+    def photo_path(instance, filename):
+        return f'{instance.place.name}/{filename}'
+
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='photos')
+    photo = models.ImageField(upload_to=photo_path)
+
+
+class Review(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300, blank=True, null=True)
+    like = models.ManyToManyField(User, related_name='like_reviews')
