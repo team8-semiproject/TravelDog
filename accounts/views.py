c4fc7751901322a54455d25932dd00b345678abd
Lightness-of-Being <gje960217@naver.com>
2023-05-07 15:41:15 +0900

    delete pagination

--- accounts/views.py
+++ accounts/views.py
@@ -58,34 +58,30 @@ def profile(request, username):
     
     # for 내가 쓴 리뷰
     my_reviews = Review.objects.filter(user=person.pk)
-    page1 = request.GET.get('page', '1')
-    per_page1 = 8
-    paginator1 = Paginator(my_reviews, per_page1)
-    page_object1 = paginator1.get_page(page1)
+    # page1 = request.GET.get('page', '1')
+    # per_page1 = 8
+    # paginator1 = Paginator(my_reviews, per_page1)
+    # page_object1 = paginator1.get_page(page1)
     
     # for 북마크
     bookmark_places = person.bookmarked_places.all()
-    page2 = request.GET.get('page', '1')
-    per_page2 = 12
-    paginator2 = Paginator(bookmark_places, per_page2)
-    page_object2 = paginator2.get_page(page2)
+    # page2 = request.GET.get('page', '1')
+    # per_page2 = 12
+    # paginator2 = Paginator(bookmark_places, per_page2)
+    # page_object2 = paginator2.get_page(page2)
     
     # for 좋아요한 리뷰
     reviews_like = person.like_reviews.all()
-    print('like_reviews',reviews_like)
-    page3 = request.GET.get('page', '1')
-    per_page3 = 8
-    paginator3 = Paginator(reviews_like, per_page3)
-    page_object3 = paginator3.get_page(page3)
+    # page3 = request.GET.get('page', '1')
+    # per_page3 = 8
+    # paginator3 = Paginator(reviews_like, per_page3)
+    # page_object3 = paginator3.get_page(page3)
 
     context = {
         'person': person,
-        'my_reviews': page_object1,
-        'bookmark_places' : page_object2,
-        'like_reviews' : page_object3,
-        'range1': ['1', '2', '3', '4', '5'],
-        'range2': ['1', '2', '3', '4', '5'],
-        'range3': ['1', '2', '3', '4', '5'],
+        'my_reviews': my_reviews,
+        'bookmark_places' : bookmark_places,
+        'like_reviews' : reviews_like,
     }
     
     # for update_profile modal
